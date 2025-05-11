d959c777d354c6d8c943b7c64d26daa9204ca09d
Isaac Alarcon <alarconcordovaramses@gmail.com>
2025-05-11 23:15:39 +0200

    wip: remove legacy kotlin plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,13 @@ rootProject.name = "ktor-hexagonal-multimodule"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-include(
-    "app:infra",
-    "app:core",
-    "app:common",
-    "app:adapters:env",
-    "app:adapters:persist",
-    "app:adapters:remoting",
-    "app:adapters:primary-web",
-)
+// TODO: Restore include when build-logic is implemented
+//include(
+//    "app:infra",
+//    "app:core",
+//    "app:common",
+//    "app:adapters:env",
+//    "app:adapters:persist",
+//    "app:adapters:remoting",
+//    "app:adapters:primary-web",
+//)
